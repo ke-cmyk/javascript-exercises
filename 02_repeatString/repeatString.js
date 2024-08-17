762e8c73fe4361f13364e0ed63293387ff9f9a93
Kevin Le <60284493+ke-cmyk@users.noreply.github.com>
2024-08-17 13:57:37 +0700

    Complete 01, 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
-};
+const repeatString = function(input, reps) {
+    let repeated = "";
+    for (let i=0;i<reps;i++) {
+        repeated += input;
+    }
+    return repeated;
+};  
 
 // Do not edit below this line
 module.exports = repeatString;
