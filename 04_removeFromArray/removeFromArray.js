b8a0b8e3c821001e69699b6d5a309a1e4f4eb947
Kevin Le <60284493+ke-cmyk@users.noreply.github.com>
2024-08-18 17:13:09 +0700

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...targets) {
+    let newArray = [];
+
+    targetLength = targets.length;
+    arrayLength = array.length;
+
+    for (let i = 0; i < arrayLength; i++) {
+        let flag = false;
+        for (let j = 0; j < targetLength; j++) {
+            if (array[i] === targets[j]) {
+                flag = true;
+            }
+        }
+        if (flag === true) {
+            continue;
+        }
+        newArray.push(array[i]);
+        }
+
+    return newArray;
+}
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
