53ba0e4a2a3492d5b9ce0e764ffd7cb11c5c526a
Kevin Le <60284493+ke-cmyk@users.noreply.github.com>
2024-08-18 15:55:48 +0700

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-    
+const reverseString = function(string) {
+    splitString = string.split("")
+    let reversedString = ''
+    for (let i = (splitString.length - 1);i > -1;i--) {
+        reversedString += splitString[i];
+    }
+    return reversedString;
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
