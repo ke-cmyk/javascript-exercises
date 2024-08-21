b56772587680d8297bf8d16564150a90271110d5
Kevin Le <60284493+ke-cmyk@users.noreply.github.com>
2024-08-21 23:07:06 +0700

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesFahrenheit) {
+  let celsius = (degreesFahrenheit - 32) * (5/9)
+  celsius = Number(celsius.toFixed(1));
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesCelsius) {
+  let fahrenheit = (degreesCelsius * 9/5 + 32)
+  fahrenheit = Number(fahrenheit.toFixed(1));
+  return fahrenheit;
 };
 
 // Do not edit below this line
