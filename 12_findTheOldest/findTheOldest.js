2c89fa7f7886918bf6b472801be40f21386958db
Kevin Le <60284493+ke-cmyk@users.noreply.github.com>
2024-08-22 18:28:22 +0700

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,45 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestPerson = {};
+    let oldestAge = 0;
+    let currentYear = new Date().getFullYear();
+    for (const person of people) {
+        if ('yearOfDeath' in person) {
+            let age = person['yearOfDeath'] - person['yearOfBirth'];
+            if (age > oldestAge) {
+                oldestPerson = person;
+                oldestAge = age;
+            }
+        }
+        else {
+            let age = currentYear - person['yearOfBirth'];
+            if (age > oldestAge) {
+                oldestPerson = person;
+                oldestAge = age;
+            }
+        }
+    }
+    return oldestPerson;
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1066,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
