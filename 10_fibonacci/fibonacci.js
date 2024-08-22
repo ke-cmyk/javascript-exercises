1435fafe4697c973a59094747dba56d9bfb4fda1
Kevin Le <60284493+ke-cmyk@users.noreply.github.com>
2024-08-22 17:34:44 +0700

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(size) {
+    if (size < 0) {
+        return 'OOPS';
+    }
+    fiboArray = [0, 1]
+    for (let i = 0; i < size; i++) {
+        fiboArray.push(fiboArray[i] + fiboArray[(i + 1)]);
+    }
+    return(fiboArray[size]);
 };
+
+fibonacci(6);
 
 // Do not edit below this line
 module.exports = fibonacci;
