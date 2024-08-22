caca95a1b7a8c0ffb11dc29e65aaa9514beda13b
Kevin Le <60284493+ke-cmyk@users.noreply.github.com>
2024-08-22 17:06:03 +0700

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = stripPunctuation(string).toLowerCase();
+    backwardsString = ''
+    for (let i = (string.length - 1); i >= 0; i--) {
+        backwardsString += string.charAt(i);
+    }
+    return string === backwardsString;
 };
+
+const stripPunctuation = function (string) {
+    return string.replace(/[,.!? ]/g, '');
+}
+
+palindromes('racecar');
 
 // Do not edit below this line
 module.exports = palindromes;
