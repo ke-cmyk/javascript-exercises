dd0a17b9775aee0e525e05b8e337028180295919
Kevin Le <60284493+ke-cmyk@users.noreply.github.com>
2024-08-22 16:08:46 +0700

    Complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,47 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arraytoSum) {
+  let sum = 0;
+  for(const num of arraytoSum) {
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arraytoMultiply) {
+  let prod = 1;
+  for(const num of arraytoMultiply) {
+    prod *= num;
+  }
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let powerResult = num1;
+  for (let i = 1; i < num2; i++) {
+    powerResult *= num1;
+  } 
+	return powerResult;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+  let factorial = num;
+  for (let i = (num-1) ;i > 0; i--) {
+    factorial *= i; 
+  }
+  return factorial;
 };
+
+factorial(0);
 
 // Do not edit below this line
 module.exports = {
