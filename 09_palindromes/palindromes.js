c23fb3f6fd7def06327571092a638fad747acc43
Kevin Le <60284493+ke-cmyk@users.noreply.github.com>
2024-09-11 23:35:52 +0700

    Refactor 09_palndromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,14 +1,29 @@
-const palindromes = function (string) {
-    string = stripPunctuation(string).toLowerCase();
-    backwardsString = ''
-    for (let i = (string.length - 1); i >= 0; i--) {
-        backwardsString += string.charAt(i);
-    }
-    return string === backwardsString;
-};
+// const palindromes = function (string) {
+//     string = stripPunctuation(string).toLowerCase();
+//     backwardsString = ''
+//     for (let i = (string.length - 1); i >= 0; i--) {
+//         backwardsString += string.charAt(i);
+//     }
+//     return string === backwardsString;
+// };
 
-const stripPunctuation = function (string) {
-    return string.replace(/[,.!? ]/g, '');
+// const stripPunctuation = function (string) {
+//     return string.replace(/[,.!? ]/g, '');
+// }
+
+const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789'
+
+function palindromes(string) {
+    const stringArray = string.toLowerCase()
+    .split('')
+    .filter((char) => alphanumerical.includes(char))
+    .join('');
+
+    const backwardsStringArray = stringArray.split('')
+    .reverse()
+    .join('');
+
+    return stringArray == backwardsStringArray;
 }
 
 palindromes('racecar');
