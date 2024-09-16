f6a822284ba855e512e74861304533bd330179a6
Kevin Le <60284493+ke-cmyk@users.noreply.github.com>
2024-09-17 00:54:15 +0700

    Refactor 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,16 +1,25 @@
-const findTheOldest = function(people) {
-    let oldestPerson = {};
-    let oldestAge = 0;
-    let currentYear = new Date().getFullYear();
-    for (const person of people) {
-      let age = ('yearOfDeath' in person ? person['yearOfDeath'] : currentYear) - person['yearOfBirth'];
-      if (age > oldestAge) {
-        oldestPerson = person;
-        oldestAge = age;
-      }
-    }
-    return oldestPerson;
-};
+// const findTheOldest = function(people) {
+//     let oldestPerson = {};
+//     let oldestAge = 0;
+//     let currentYear = new Date().getFullYear();
+//     for (const person of people) {
+//       let age = ('yearOfDeath' in person ? person['yearOfDeath'] : currentYear) - person['yearOfBirth'];
+//       if (age > oldestAge) {
+//         oldestPerson = person;
+//         oldestAge = age;
+//       }
+//     }
+//     return oldestPerson;
 
+// };
+
+let currentYear = new Date().getFullYear();
+const findTheOldest = function(people) {
+  return people.reduce((acc, currentPerson) => {
+    let currentAge = ('yearOfDeath' in currentPerson ? currentPerson['yearOfDeath'] : currentYear) - currentPerson['yearOfBirth'];
+    let accAge = ('yearOfDeath' in acc ? acc['yearOfDeath'] : currentYear) - acc['yearOfBirth'];
+    return accAge < currentAge ? currentPerson : acc;
+  })
+};
 // Do not edit below this line
 module.exports = findTheOldest;
